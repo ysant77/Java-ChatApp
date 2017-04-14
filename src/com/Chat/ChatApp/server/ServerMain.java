0a60b7392393b3ac707871b7263d1c026cbaefa8
Yatharth <ysant77@gmail.com>
2017-04-14 19:07:13 +0530

    Chat Application

---
+++ src/com/Chat/ChatApp/server/ServerMain.java
@@ -0,0 +1,30 @@
+package com.Chat.ChatApp.server;
+
+import com.Chat.ChatApp.server.Server;
+import com.Chat.ChatApp.server.ServerMain;
+
+public class ServerMain
+{
+
+	private int port;
+	private Server server;
+
+	public ServerMain(int port) 
+	{
+		this.port = port;
+		server = new Server(port);
+	}
+
+	public static void main(String[] args) 
+	{
+		int port;
+		if (args.length != 1) 
+		{
+			System.out.println("Usage: java -jar ChernoChatServer.jar [port]");
+			return;
+		}
+		port = Integer.parseInt(args[0]);
+		new ServerMain(port);
+	}
+
+}
